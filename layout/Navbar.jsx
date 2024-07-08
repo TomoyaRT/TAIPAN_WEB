39c9a26f9e5af801dc043e9c827154e2f9d7d2f7
Roger <worth22focus3000@gmail.com>
2024-07-08 16:57:31 +0800

    feat: navbar component

--- layout/Navbar.jsx
+++ layout/Navbar.jsx
@@ -17,7 +17,7 @@ import { Button } from "@nextui-org/button";
 const menuContent = [
   {
     id: 1,
-    text: "關於我們",
+    text: "首頁",
   },
   {
     id: 2,
@@ -25,7 +25,7 @@ const menuContent = [
   },
   {
     id: 3,
-    text: "イベント情報",
+    text: "活動情報",
   },
   {
     id: 4,
@@ -33,68 +33,72 @@ const menuContent = [
   },
   {
     id: 5,
-    text: "お問い合わせ",
+    text: "聯絡我們",
   },
 ];
 
-export const LayoutNavbar = () => {
+export default function LayoutNavbar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const [isMenuActive, setMenuActive] = React.useState(2);
+  const [isMenuActive, setMenuActive] = React.useState(1);
 
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen}>
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
-        <NavbarBrand>
-          <Image
-            priority
-            alt="TAIPAN Logo"
-            height={200}
-            src="/Logo.png"
-            style={{ width: "auto", height: "auth" }}
-            width={200}
+    <>
+      <Navbar className="custom_navbar" onMenuOpenChange={setIsMenuOpen}>
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="menu_toggle"
           />
-        </NavbarBrand>
-      </NavbarContent>
+          <NavbarBrand>
+            <Image
+              priority
+              alt="TAIPAN Logo"
+              height={200}
+              src="/Logo.png"
+              style={{ width: "auto", height: "auth" }}
+              width={200}
+            />
+          </NavbarBrand>
+        </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-3" justify="center">
-        {menuContent.map((item) => (
-          <NavbarItem key={item.id}>
-            <Link
-              color={item.id == isMenuActive ? "primary" : "foreground"}
-              href="#"
-              onClick={() => setMenuActive(item.id)}
-            >
-              {item.text}
-            </Link>
+        <NavbarContent className="hidden navbar_content gap-3" justify="center">
+          {menuContent.map((item) => (
+            <NavbarItem key={item.id}>
+              <Link
+                color={item.id == isMenuActive ? "primary" : "foreground"}
+                href="#"
+                isDisabled={item.id !== 1}
+                onClick={() => setMenuActive(item.id)}
+              >
+                {item.text}
+              </Link>
+            </NavbarItem>
+          ))}
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <Button as={Link} color="primary" href="#" variant="flat">
+              會員登入
+            </Button>
           </NavbarItem>
-        ))}
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            會員登入
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarMenu>
-        {menuContent.map((item) => (
-          <NavbarMenuItem key={item.id}>
-            <Link
-              className="w-full"
-              color={item.id == isMenuActive ? "primary" : "foreground"}
-              href="#"
-              size="lg"
-              onClick={() => setMenuActive(item.id)}
-            >
-              {item.text}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
+        </NavbarContent>
+        <NavbarMenu>
+          {menuContent.map((item) => (
+            <NavbarMenuItem key={item.id}>
+              <Link
+                className="w-full"
+                color={item.id == isMenuActive ? "primary" : "foreground"}
+                href="#"
+                isDisabled={item.id !== 1}
+                size="lg"
+                onClick={() => setMenuActive(item.id)}
+              >
+                {item.text}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
+    </>
   );
-};
+}
