ca19384e120c1a8f9aea5bb68db03517e1d6ad70
Roger <worth22focus3000@gmail.com>
2024-07-09 13:55:58 +0800

    feat: modify default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <LayoutNavbar />
           <main>{children}</main>
           <Footer />
